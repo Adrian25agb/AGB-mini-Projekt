8f52c2263049466aa2d5179acb38adeb0aea7d0b
Adrian25agb <adrian.schindler@gmx.net>
2025-04-10 09:15:14 +0200

    mini Projekt 6

--- mini projekt 6.py
+++ mini projekt 6.py
@@ -1,17 +1,39 @@
 import random
-spieler = random.randint(1,6)
-computer = random.randint(1,6)
-print("spieler: " + str(spieler))
-print("computer: " + str(computer))
-if computer > spieler:
-    print("computer wins")
-if computer < spieler:
-    print("spieler wins")
-    while computer == 6:
-        computer = computer + random.randint(1,6)
-if computer == spieler:
-    print("gleiche zahl")
-    spieler = random.randint(1,6)
-    computer = random.randint(1,6)
+
+runde = 1
+bestOfFive = 0
+
+while runde <= 5:  # Es sollen maximal 5 Runden gespielt werden
+    print("\nRunde " + str(runde) + "\n")
     
-        
+    spieler = random.randint(1, 6)
+    computer = random.randint(1, 6)
+    
+    print("Spieler: " + str(spieler))
+    print("Computer: " + str(computer))
+    
+    # Wenn der Spieler eine 6 würfelt, bekommt er eine zusätzliche Zahl
+    if spieler == 6:
+        spieler += random.randint(1, 6)
+        print("Spieler würfelt eine 6 und bekommt eine zusätzliche Zahl: " + str(spieler))
+    
+    # Wenn der Computer eine 6 würfelt, bekommt er eine zusätzliche Zahl
+    if computer == 6:
+        computer += random.randint(1, 6)
+        print("Computer würfelt eine 6 und bekommt eine zusätzliche Zahl: " + str(computer))
+    
+    # Bestimmen des Siegers
+    if computer > spieler:
+        print("Computer gewinnt diese Runde")
+        bestOfFive += 1
+    elif computer < spieler:
+        print("Spieler gewinnt diese Runde")
+        bestOfFive += 1
+    else:
+        print("Unentschieden! Beide haben die gleiche Zahl geworfen.")
+    
+    runde += 1
+
+# Ausgabe nach den 5 Runden
+print("\nDas Spiel ist vorbei! Nach 5 Runden war der Stand:")
+print("Computer-Siege: " + str(bestOfFive))
